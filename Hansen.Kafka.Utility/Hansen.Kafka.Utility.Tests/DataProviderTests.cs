1702c79950e83f4a48a5986f5c895f6052714c61
SM <temp.smalldragon@gmail.com>
2020-10-04 15:47:09 +0700

    Update unit tests

--- Hansen.Kafka.Utility/Hansen.Kafka.Utility.Tests/DataProviderTests.cs
+++ Hansen.Kafka.Utility/Hansen.Kafka.Utility.Tests/DataProviderTests.cs
@@ -1,10 +1,9 @@
-﻿using Hansen.Kafka.Utility.Models;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using System.Linq;
-using Hansen.Kafka.Utility.Configuration;
+﻿using Hansen.Kafka.Utility.Configuration;
 using Hansen.Kafka.Utility.Providers;
 using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
 
 namespace Hansen.Kafka.Utility.Tests
 {
@@ -22,7 +21,6 @@ namespace Hansen.Kafka.Utility.Tests
             KafkaDataProvider dataProvider = new KafkaDataProvider(config, LogManager.GetLogger(typeof(DataProviderTests)));
             var topics = dataProvider.GetTopicsAsync().Result.ToList();
             Assert.IsTrue(topics.Count > 0);
-            Assert.AreEqual(topics.First().Name, "sm-message");
         }
         [TestMethod]
         public void GetListMessagePerTopics_ReturnList()
@@ -34,6 +32,44 @@ namespace Hansen.Kafka.Utility.Tests
             KafkaDataProvider dataProvider = new KafkaDataProvider(config, LogManager.GetLogger(typeof(DataProviderTests)));
             var messages = dataProvider.GetMessagesAsync("sm-message").Result.ToList();
             Assert.IsTrue(messages.Count > 0);
+        }
+        [TestMethod]
+        public void CreateNewTopicWith1Partition_ReturnSuccess()
+        {
+            ConnectionConfiguration config = new ConnectionConfiguration()
+            {
+                BootstrapServer = "localhost:9092"
+            };
+            KafkaDataProvider dataProvider = new KafkaDataProvider(config, LogManager.GetLogger(typeof(DataProviderTests)));
+            string newTopic = $"UnitTest-{Guid.NewGuid().ToString()}-{DateTime.Now.Ticks}";
+            var task = dataProvider.AddNewTopicAsync(newTopic, 1, -1);
+            task.Wait();
+            // get all topics
+            var topics = dataProvider.GetTopicsAsync().Result.ToList();
+            Assert.IsTrue(topics.Count > 0);
+            var nt = topics.FirstOrDefault(x => x.Name == newTopic);
+            Assert.IsTrue(nt != null);
+            Assert.IsTrue(nt.Partitions.First().Id == 0);
+            // create topic with 2 partition
+        }
+        [TestMethod]
+        public void CreateNewTopicWith3Partition_ReturnSuccess()
+        {
+            ConnectionConfiguration config = new ConnectionConfiguration()
+            {
+                BootstrapServer = "localhost:9092"
+            };
+            KafkaDataProvider dataProvider = new KafkaDataProvider(config, LogManager.GetLogger(typeof(DataProviderTests)));
+            string newTopic = $"UnitTest-{Guid.NewGuid().ToString()}-{DateTime.Now.Ticks}";
+            var task = dataProvider.AddNewTopicAsync(newTopic, 3, -1);
+            task.Wait();
+            // get all topics
+            var topics = dataProvider.GetTopicsAsync().Result.ToList();
+            Assert.IsTrue(topics.Count > 0);
+            var nt = topics.FirstOrDefault(x => x.Name == newTopic);
+            Assert.IsTrue(nt != null);
+            Assert.IsTrue(nt.Partitions.Count == 3);
+            // create topic with 2 partition
         }
     }
 }
