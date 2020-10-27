e8ee6c5ef220dbe2a1ecd1b3b01e809d27ac37ef
SM\SM <temp.smalldragon@gmail.com>
2020-10-27 15:08:32 +0700

    no message

--- SM.Ecommerce/SM.Ecommerce.Api.Customers/Models/CustomerDto.cs
+++ SM.Ecommerce/SM.Ecommerce.Api.Customers/Models/CustomerDto.cs
@@ -9,6 +9,6 @@ namespace SM.Ecommerce.Api.Customers.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Address { get; set; }
+        public string Address { get; set; }
     }
 }
